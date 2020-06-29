f69e919c7e37c0083273e190c8061a75dc065d53
mario robayo <marioferyhwh@gmail.com>
2020-06-29 17:19:14 -0500

    creacion de actividad d einspeccion

---
+++ app/src/main/java/com/example/asistentedocumental/Inspection.kt
@@ -0,0 +1,11 @@
+package com.example.asistentedocumental
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Inspection : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inspection)
+    }
+}
