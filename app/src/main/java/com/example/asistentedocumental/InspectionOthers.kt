a302a30cdb54a692d570ffa104c36cd848d6cc4c
mario robayo <marioferyhwh@gmail.com>
2020-06-29 17:25:35 -0500

    se crea actividad otros de inspeccion

---
+++ app/src/main/java/com/example/asistentedocumental/InspectionOthers.kt
@@ -0,0 +1,11 @@
+package com.example.asistentedocumental
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InspectionOthers : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inspection_others)
+    }
+}
