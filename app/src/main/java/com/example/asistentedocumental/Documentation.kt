25984b19e014793fdaf572d6198253a0b66bbbc0
mario robayo <marioferyhwh@gmail.com>
2020-06-30 10:53:02 -0500

    cracion de home activity

--- app/src/main/java/com/example/asistentedocumental/Documentation.kt
+++ app/src/main/java/com/example/asistentedocumental/Documentation.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_documentation.*
 
-class Documentation : AppCompatActivity() {
+class DocumentationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_documentation)
