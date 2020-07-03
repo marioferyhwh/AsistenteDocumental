06e4d64f0f028ca7b30496637fb2af75e2250d07
mario robayo <marioferyhwh@gmail.com>
2020-07-03 12:09:44 -0500

    renombra strings  y se realiza prueanas de lstado

--- app/src/main/java/com/example/asistentedocumental/adapters/ListQuestType1Adapter.kt
+++ app/src/main/java/com/example/asistentedocumental/adapters/ListQuestType1Adapter.kt
@@ -4,7 +4,9 @@ import android.view.LayoutInflater
 import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Spinner
 import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.example.asistentedocumental.R
 import com.example.asistentedocumental.models.QuestType1
@@ -30,11 +32,18 @@ class ListQuestType1Adapter(val quests: ArrayList<QuestType1>) :
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun binbQuest(questSelect: QuestType1) {
-            var quest = itemView.findViewById<TextView>(R.id.testview_quest)
+            var id = itemView.findViewById<TextView>(R.id.testview_id)
+            var quest = itemView.findViewById<TextView>(R.id.testview_ques)
             var retie = itemView.findViewById<TextView>(R.id.testview_retie)
-
+            var spinner = itemView.findViewById<Spinner>(R.id.spinner)
+            id.text = questSelect.id.toString()
             quest.text = questSelect.quest.toString()
             retie.text = questSelect.Retie.toString()
+
+            if (retie.text === "") {
+                retie.visibility = View.GONE
+                spinner.visibility = View.GONE
+            }
         }
 
     }
