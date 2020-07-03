1ccde2625b8969a885b33ee839a3623182d3299b
mario robayo <marioferyhwh@gmail.com>
2020-07-03 12:39:05 -0500

    se crea xml para mostran de una segunda manera la informacion

--- app/src/main/java/com/example/asistentedocumental/models/QuestType1.kt
+++ app/src/main/java/com/example/asistentedocumental/models/QuestType1.kt
@@ -1,4 +1,4 @@
 package com.example.asistentedocumental.models
 
-data class QuestType1 (val id:String="",val quest:String,val Retie:String,val answer:Int=0){
+data class QuestType1 (val id:String="",val quest:String,val Retie:String,var answer:Int=0){
 }
---
+++ app/src/main/java/com/example/asistentedocumental/models/QuestType1Group.kt
@@ -0,0 +1,4 @@
+package com.example.asistentedocumental.models
+
+class QuestType1Group(val title:String,var questGroup: ArrayList<QuestType1> ) {
+}
