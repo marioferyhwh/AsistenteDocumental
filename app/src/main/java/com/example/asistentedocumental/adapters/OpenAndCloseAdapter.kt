04e9c6f91c49f3130a190999bd4c7f59b354d413
mario robayo <marioferyhwh@gmail.com>
2020-07-16 10:20:41 -0500

    se cambia texto y se modifica las descripciones de open y close

--- app/src/main/java/com/example/asistentedocumental/adapters/OpenAndCloseAdapter.kt
+++ app/src/main/java/com/example/asistentedocumental/adapters/OpenAndCloseAdapter.kt
@@ -29,10 +29,10 @@ class OpenAndCloseAdapter(val conformes: ArrayList<OpenAndClose>) :
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun binbQuest(noconforme: OpenAndClose) {
-            var id = itemView.findViewById<TextView>(R.id.textview_id)
+            var descripcion = itemView.findViewById<TextView>(R.id.textview_id)
             var retie = itemView.findViewById<TextView>(R.id.textview_ret)
             var cierre = itemView.findViewById<TextView>(R.id.textview_close)
-            id.text = noconforme.id.toString()
+            descripcion.text = noconforme.descripcion
             retie.text = noconforme.retie
             cierre.text = noconforme.cierre
         }
